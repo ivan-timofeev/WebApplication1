e0f3ebf970d99256976d1c055accd4a4c4c32a4d
Ivan Timofeev <qwark4media@gmail.com>
2023-03-01 21:48:04 +0300

    Draft#2

--- WebApplication1/Abstraction/Services/IShoppingCart.cs
+++ WebApplication1/Abstraction/Services/IShoppingCart.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using WebApplication1.Abstraction.Models;
+using WebApplication1.Models;
 
 namespace WebApplication1.Abstraction.Services;
 
@@ -7,13 +9,37 @@ public interface IShoppingCart
     ShoppingCartVm GetShoppingCart(Guid customerId);
     void StartNewShoppingSession(Guid customerId);
     void StartNewShoppingSession(Guid customerId, ShoppingCartVm model);
-    void UpdateItem(Guid customerId, ShoppingCartUpdateItemVm model);
+    void UpdateItem(Guid customerId, ShoppingCartItemUpdateVm model);
+    void ReleaseCart(Guid customerId);
 }
 
-public record ShoppingCartUpdateItemVm
+public class ShoppingCart : DomainModel
+{
+    public Guid CustomerId { get; set; }
+    public Customer Customer { get; set; }
+    public List<ShoppingCartItem> CartItems { get; set; }
+
+    public ShoppingCart()
+    {
+        Customer = null!;
+        CartItems = new List<ShoppingCartItem>();
+    }
+}
+
+public class ShoppingCartItem : DomainModel
+{
+    public Guid SaleItemId { get; set; }
+    public SaleItem SaleItem { get; set; }
+    public int Quantity { get; set; }
+
+    public ShoppingCartItem()
+    {
+        SaleItem = null!;
+    }
+}
+
+public record ShoppingCartItemUpdateVm
 (
-    [Required]
-    Guid SalePointId,
     [Required]
     Guid SaleItemId,
     [Range(0, 1000, ErrorMessage = "Value must be in the range [0, 1000]")]
@@ -28,7 +54,7 @@ public record ShoppingCartVm
 
 public record ShoppingCartItemVm
 (
-    Guid SalePointId,
     Guid SaleItemId,
-    int Quantity
+    int Quantity,
+    int AvailableQuantity
 );
